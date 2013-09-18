20bdbc176d8f179ed23b4545ecdba84a24a1b9ba
scorpiozj <scorpiozj@gmail.com>
2013-09-18 09:57:02 +0800

    Add new class

---
+++ iOS7NewFeature/DynamicBehaviorsSample/DynamicBehaviorsSample/ZJDynamicViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJDynamicViewController.h
+//  DynamicBehaviorsSample
+//
+//  Created by Zhu J on 9/18/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJDynamicViewController : UIViewController
+
+@end
