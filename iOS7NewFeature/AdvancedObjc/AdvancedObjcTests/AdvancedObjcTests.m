3341250d15fc3cc1e6382ff46424a6816f33f7af
scorpiozj <scorpiozj@gmail.com>
2013-09-22 13:36:45 +0800

    add new project: AdvancedObjc

---
+++ iOS7NewFeature/AdvancedObjc/AdvancedObjcTests/AdvancedObjcTests.m
@@ -0,0 +1,34 @@
+//
+//  AdvancedObjcTests.m
+//  AdvancedObjcTests
+//
+//  Created by Zhu J on 9/22/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdvancedObjcTests : XCTestCase
+
+@end
+
+@implementation AdvancedObjcTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
