7434281e39443c4df84e4f9ef453f8360f0dd54a
scorpiozj <scorpiozj@gmail.com>
2013-09-24 14:21:47 +0800

    Add New Class

---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJBouncyFallBehavior.h
@@ -0,0 +1,13 @@
+//
+//  ZJBouncyFallBehavior.h
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 9/24/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJBouncyFallBehavior : UIDynamicBehavior
+
+@end
---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJBouncyFallBehavior.m
@@ -0,0 +1,13 @@
+//
+//  ZJBouncyFallBehavior.m
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 9/24/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJBouncyFallBehavior.h"
+
+@implementation ZJBouncyFallBehavior
+
+@end
