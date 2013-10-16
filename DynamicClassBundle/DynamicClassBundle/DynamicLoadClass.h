bcd2988d5b1ed7231fcf34ada856548ae0c0359a
scorpiozj <scorpiozj@gmail.com>
2013-10-16 13:47:27 +0800

    Add Principal Class

---
+++ DynamicClassBundle/DynamicClassBundle/DynamicLoadClass.h
@@ -0,0 +1,13 @@
+//
+//  DynamicLoadClass.h
+//  DynamicClassBundle
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DynamicLoadClass : NSObject
+- (void)print;
+@end
---
+++ DynamicClassBundle/DynamicClassBundle/DynamicLoadClass.m
@@ -0,0 +1,16 @@
+//
+//  DynamicLoadClass.m
+//  DynamicClassBundle
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "DynamicLoadClass.h"
+
+@implementation DynamicLoadClass
+- (void)print
+{
+    NSLog(@"this a dynamic loaded class!");
+}
+@end
