b52e7a92aa2f3acbb6abfb1fe2db495dfc7f6158
scorpiozj <scorpiozj@gmail.com>
2013-10-16 12:49:56 +0800

    Add New Class

---
+++ RuntimeStudy/RuntimeStudy/MyClass.h
@@ -0,0 +1,13 @@
+//
+//  MyClass.h
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClass : NSObject
+
+@end
---
+++ RuntimeStudy/RuntimeStudy/MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
