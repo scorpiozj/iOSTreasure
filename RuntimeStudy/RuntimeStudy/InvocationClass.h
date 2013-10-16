863c5cec50406f94e5c1c79bf57778a7e1daff39
scorpiozj <scorpiozj@gmail.com>
2013-10-16 14:29:40 +0800

    Add Invocation class for Massage Forwarding

---
+++ RuntimeStudy/RuntimeStudy/InvocationClass.h
@@ -0,0 +1,13 @@
+//
+//  InvocationClass.h
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InvocationClass : NSObject
+
+@end
---
+++ RuntimeStudy/RuntimeStudy/InvocationClass.m
@@ -0,0 +1,13 @@
+//
+//  InvocationClass.m
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/16/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "InvocationClass.h"
+
+@implementation InvocationClass
+
+@end
