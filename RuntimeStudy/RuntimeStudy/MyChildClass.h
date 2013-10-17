42e8c477de18d925ab2f51d34af1861c707b8ce0
scorpiozj <scorpiozj@gmail.com>
2013-10-17 09:23:55 +0800

    Add MyChildClass

---
+++ RuntimeStudy/RuntimeStudy/MyChildClass.h
@@ -0,0 +1,13 @@
+//
+//  MyChildClass.h
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/17/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@interface MyChildClass : MyClass
+
+@end
---
+++ RuntimeStudy/RuntimeStudy/MyChildClass.m
@@ -0,0 +1,13 @@
+//
+//  MyChildClass.m
+//  RuntimeStudy
+//
+//  Created by Zhu J on 10/17/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "MyChildClass.h"
+
+@implementation MyChildClass
+
+@end
