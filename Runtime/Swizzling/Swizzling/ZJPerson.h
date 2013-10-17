b10356b7f7b8facce20204edf87127a966739c21
scorpiozj <scorpiozj@gmail.com>
2013-10-17 11:11:13 +0800

    Add ZJPerson

---
+++ Runtime/Swizzling/Swizzling/ZJPerson.h
@@ -0,0 +1,14 @@
+//
+//  ZJPerson.h
+//  Swizzling
+//
+//  Created by Zhu J on 10/17/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJPerson : NSObject
+@property (nonatomic, strong) NSString *familyName;
+@property (nonatomic, strong) NSString *lastName;
+@end
---
+++ Runtime/Swizzling/Swizzling/ZJPerson.m
@@ -0,0 +1,13 @@
+//
+//  ZJPerson.m
+//  Swizzling
+//
+//  Created by Zhu J on 10/17/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJPerson.h"
+
+@implementation ZJPerson
+
+@end
