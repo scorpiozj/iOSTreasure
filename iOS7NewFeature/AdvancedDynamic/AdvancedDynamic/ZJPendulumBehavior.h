2fe08489be3bb06b3ba26245392fd679bbb146c6
scorpiozj <scorpiozj@gmail.com>
2013-10-22 15:11:16 +0800

    Add class

---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJPendulumBehavior.h
@@ -0,0 +1,13 @@
+//
+//  ZJPendulumBehavior.h
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 10/22/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJPendulumBehavior : UIDynamicBehavior
+
+@end
---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJPendulumBehavior.m
@@ -0,0 +1,13 @@
+//
+//  ZJPendulumBehavior.m
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 10/22/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJPendulumBehavior.h"
+
+@implementation ZJPendulumBehavior
+
+@end
