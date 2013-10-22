6a928c896cd5d86787fb92ad5c9be0c8582a3331
scorpiozj <scorpiozj@gmail.com>
2013-10-22 16:38:37 +0800

    initial Add

---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJDynamicitemsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZJDynamicitemsViewController.m
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 10/22/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJDynamicitemsViewController.h"
+
+@interface ZJDynamicitemsViewController ()
+
+@end
+
+@implementation ZJDynamicitemsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
