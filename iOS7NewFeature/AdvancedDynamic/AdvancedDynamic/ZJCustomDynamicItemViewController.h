3f8baacfb0be41e7533cd24ad45d55d8b5a24a00
scorpiozj <scorpiozj@gmail.com>
2013-10-23 10:11:07 +0800

    initial Add

---
+++ iOS7NewFeature/AdvancedDynamic/AdvancedDynamic/ZJCustomDynamicItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJCustomDynamicItemViewController.h
+//  AdvancedDynamic
+//
+//  Created by Zhu J on 10/23/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJCustomDynamicItemViewController : UIViewController
+
+@end
