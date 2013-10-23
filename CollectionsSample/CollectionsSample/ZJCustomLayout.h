4b3cb04160bdd8abc550b4779c43263ddaa8a763
scorpiozj <scorpiozj@gmail.com>
2013-10-23 16:31:30 +0800

    Initial Add

---
+++ CollectionsSample/CollectionsSample/ZJCustomLayout.h
@@ -0,0 +1,13 @@
+//
+//  ZJCustomLayout.h
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/23/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJCustomLayout : UICollectionViewLayout
+
+@end
---
+++ CollectionsSample/CollectionsSample/ZJCustomLayout.m
@@ -0,0 +1,13 @@
+//
+//  ZJCustomLayout.m
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/23/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJCustomLayout.h"
+
+@implementation ZJCustomLayout
+
+@end
