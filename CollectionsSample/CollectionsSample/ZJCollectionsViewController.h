657a911d869e6c71d18b57d8834b31c206cd538f
scorpiozj <scorpiozj@gmail.com>
2013-10-23 14:39:05 +0800

    initial Add

---
+++ CollectionsSample/CollectionsSample/ZJCollectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJCollectionsViewController.h
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/23/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJCollectionsViewController : UIViewController
+
+@end
---
+++ CollectionsSample/CollectionsSample/ZJCollectionsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZJCollectionsViewController.m
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/23/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJCollectionsViewController.h"
+
+@interface ZJCollectionsViewController ()
+
+@end
+
+@implementation ZJCollectionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
