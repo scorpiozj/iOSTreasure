5344025719bf374d0374c3cf94c4e5f0130c7a27
scorpiozj <scorpiozj@gmail.com>
2013-10-29 10:52:07 +0800

    Add New Class

---
+++ CollectionsSample/CollectionsSample/ZJCustomCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZJCustomCollectionViewController.m
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/29/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJCustomCollectionViewController.h"
+
+@interface ZJCustomCollectionViewController ()
+
+@end
+
+@implementation ZJCustomCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
