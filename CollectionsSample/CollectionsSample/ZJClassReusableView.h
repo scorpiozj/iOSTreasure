2cfca0289e5a6756368beff6460eb583095cf0fd
scorpiozj <scorpiozj@gmail.com>
2013-10-30 09:04:56 +0800

    Add Class

---
+++ CollectionsSample/CollectionsSample/ZJClassReusableView.h
@@ -0,0 +1,13 @@
+//
+//  ZJClassReusableView.h
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/30/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJClassReusableView : UICollectionReusableView
+
+@end
---
+++ CollectionsSample/CollectionsSample/ZJClassReusableView.m
@@ -0,0 +1,31 @@
+//
+//  ZJClassReusableView.m
+//  CollectionsSample
+//
+//  Created by Zhu J on 10/30/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJClassReusableView.h"
+
+@implementation ZJClassReusableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
