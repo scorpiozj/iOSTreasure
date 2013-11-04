9121aa7c68ddc2195760a4a6e7c094998365c2cf
scorpiozj <scorpiozj@gmail.com>
2013-11-04 15:51:02 +0800

    Add New Class

---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJToViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJToViewController.h
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/4/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJToViewController : UIViewController
+
+@end
---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJToViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZJToViewController.m
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/4/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJToViewController.h"
+
+@interface ZJToViewController ()
+
+@end
+
+@implementation ZJToViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
