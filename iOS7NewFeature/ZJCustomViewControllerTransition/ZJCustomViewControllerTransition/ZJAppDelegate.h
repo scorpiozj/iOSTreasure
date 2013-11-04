c295b253ff7f7a7d09517bf22e143a6fdbd26187
scorpiozj <scorpiozj@gmail.com>
2013-11-04 10:50:38 +0800

    init ZJCustomViewControllerTransition Proj

---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZJAppDelegate.h
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/4/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/4/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZJAppDelegate class]));
+    }
+}
