74a5a63acada8d1b78c1c38f5649e96eddad268f
scorpiozj <scorpiozj@gmail.com>
2013-11-06 17:26:32 +0800

    Add New Class

---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJSliderTransition.h
@@ -0,0 +1,13 @@
+//
+//  ZJSliderTransition.h
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/6/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJSliderTransition : UIPercentDrivenInteractiveTransition
+
+@end
---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJSliderTransition.m
@@ -0,0 +1,13 @@
+//
+//  ZJSliderTransition.m
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/6/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJSliderTransition.h"
+
+@implementation ZJSliderTransition
+
+@end
