10d79b811cf3adf295b9d423dd6c4658762d7e3f
scorpiozj <scorpiozj@gmail.com>
2013-11-06 14:14:09 +0800

    Add New Class

---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJTransitionDelegateObj.h
@@ -0,0 +1,13 @@
+//
+//  ZJTransitionDelegateObj.h
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/6/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJTransitionDelegateObj : NSObject<UIViewControllerTransitioningDelegate>
+
+@end
---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJTransitionDelegateObj.m
@@ -0,0 +1,13 @@
+//
+//  ZJTransitionDelegateObj.m
+//  ZJCustomViewControllerTransition
+//
+//  Created by Zhu J on 11/6/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJTransitionDelegateObj.h"
+
+@implementation ZJTransitionDelegateObj
+
+@end
