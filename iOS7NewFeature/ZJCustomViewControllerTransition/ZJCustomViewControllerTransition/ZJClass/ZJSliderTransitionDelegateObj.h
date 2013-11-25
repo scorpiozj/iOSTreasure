a1d5597fd10c1be8a2ade0d84185b65d916a3bb3
Charles Zhu <scorpiozj@gmail.com>
2013-11-25 21:06:15 +0800

    Add SliderTransitionDelegateObj

---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJSliderTransitionDelegateObj.h
@@ -0,0 +1,13 @@
+//
+//  ZJSliderTransitionDelegateObj.h
+//  ZJCustomViewControllerTransition
+//
+//  Created by scorpiozj on 11/25/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJSliderTransitionDelegateObj : NSObject
+
+@end
---
+++ iOS7NewFeature/ZJCustomViewControllerTransition/ZJCustomViewControllerTransition/ZJClass/ZJSliderTransitionDelegateObj.m
@@ -0,0 +1,13 @@
+//
+//  ZJSliderTransitionDelegateObj.m
+//  ZJCustomViewControllerTransition
+//
+//  Created by scorpiozj on 11/25/13.
+//  Copyright (c) 2013 Zhu J. All rights reserved.
+//
+
+#import "ZJSliderTransitionDelegateObj.h"
+
+@implementation ZJSliderTransitionDelegateObj
+
+@end
